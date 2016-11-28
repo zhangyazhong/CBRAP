d7305b91133a4d831de7e32ce38a8bd5521634a7
Aaronyxt <xiaotianyu2014@gmail.com>
2016-11-28 16:17:02 +0800

    Add files via upload

---
+++ arm/Arm.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+'''Generic arm.'''
+
+__author__ = "Olivier Cappé, Aurélien Garivier"
+__version__ = "$Revision: 1.6 $"
+
+class Arm:
+    def __init__(self): self.expectation=1
+    def draw(self): pass
